715c5a966b3f9d1335f9c831f2628cb592743c66
eastanganelli <ezequielstanganelli+git@gmail.com>
2025-11-17 03:12:51 -0300

    New triggers

--- Tables/dbo.Proveedor_Tiene_Emails.sql
+++ Tables/dbo.Proveedor_Tiene_Emails.sql
@@ -7,6 +7,29 @@
 ON [PRIMARY]
 GO
 
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE TRIGGER [dbo].[TR_ValidarCorreo]
+ON [dbo].[Proveedor_Tiene_Emails]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM INSERTED i
+        WHERE i.Correo IS NULL 
+           OR TRIM(i.Correo) = '' 
+           OR i.Correo NOT LIKE '%@%.%'
+    )
+    BEGIN
+        RAISERROR('El formato de correo electrónico es inválido o el campo está vacío.', 16, 1);
+        ROLLBACK TRANSACTION;
+        
+        RETURN;
+    END
+END
+GO
+
 ALTER TABLE [dbo].[Proveedor_Tiene_Emails]
   ADD CONSTRAINT [FK_Proveedor_Tiene_Emails] FOREIGN KEY ([IDProveedor]) REFERENCES [dbo].[Proveedores] ([ID])
 GO
--- Tables/dbo.Proveedor_Tiene_Telefonos.sql
+++ Tables/dbo.Proveedor_Tiene_Telefonos.sql
@@ -7,6 +7,29 @@
 ON [PRIMARY]
 GO
 
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE TRIGGER [dbo].[TR_ValidarTelefono]
+ON [dbo].[Proveedor_Tiene_Telefonos]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM INSERTED i
+        WHERE i.Telefono IS NULL
+           OR TRIM(i.Telefono) = ''
+           OR i.Telefono LIKE '%[^0-9 -()]%' 
+           OR LEN(REPLACE(REPLACE(REPLACE(REPLACE(i.Telefono, ' ', ''), '-', ''), '(', ''), ')', '')) < 7 
+    )
+    BEGIN
+        RAISERROR('Formato de número de teléfono inválido. Use solo dígitos, espacios, guiones o paréntesis, y debe tener al menos 7 dígitos.', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END
+END
+GO
+
 ALTER TABLE [dbo].[Proveedor_Tiene_Telefonos]
   ADD CONSTRAINT [FK_Proveedor_Tiene_Telefonos] FOREIGN KEY ([IDProveedor]) REFERENCES [dbo].[Proveedores] ([ID])
 GO
